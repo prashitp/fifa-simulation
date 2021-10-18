c6a0e78d2fc6491c2edeaf7e50a4d240f666060b
Prashit Prakashbhai Patel <pr718986@dal.ca>
2021-10-17 22:30:06 -0300

    added test

---
+++ src/test/java/com/MainTest.java
@@ -0,0 +1,14 @@
+//Author - Prashit Patel
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void notNullTest() {
+        Main m = new Main();
+        assertNotNull(m);
+    }
+}
