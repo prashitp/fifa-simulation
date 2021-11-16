b3dfe14570f1d3c6184b2083e2e1d07b222711d9
Jay Patel <jy439129@dal.ca>
2021-11-16 18:37:50 -0400

    Added implementation for input and output stream

--- src/main/java/com/StandardInputStream.java
+++ src/main/java/com/StandardInputStream.java
@@ -1,37 +1,48 @@
 package com;
 
+import java.util.Scanner;
+
 /**
  * @author Jay Patel
  */
 public class StandardInputStream implements InputStream {
 
+	private Scanner scanner;
 	private static StandardInputStream inputStream;
 
-	private StandardInputStream() {
+	private StandardInputStream(Scanner scanner) {
+		this.scanner = scanner;
 	}
 
 	public static InputStream getInstance() {
-		inputStream = new StandardInputStream();
+		if (inputStream == null) {
+			inputStream = new StandardInputStream(new Scanner(System.in));
+		}
 		return inputStream;
 	}
 
 	@Override
 	public String readLine() {
-		return "";
+		return scanner.nextLine();
 	}
 
 	@Override
 	public Integer readInteger() {
-		return 0;
+		return Integer.parseInt(readLine());
 	}
 
 	@Override
 	public Double readDouble() {
-		return 0.0D;
+		return Double.parseDouble(readLine());
 	}
 
 	@Override
 	public Long readLong() {
-		return 0L;
+		return Long.parseLong(readLine());
+	}
+
+	@Override
+	protected void finalize() {
+		scanner.close();
 	}
 }
--- src/main/java/com/StandardOutputStream.java
+++ src/main/java/com/StandardOutputStream.java
@@ -1,25 +1,40 @@
 package com;
 
+import java.io.PrintStream;
+
 /**
  * @author Jay Patel
  */
 public class StandardOutputStream implements OutputStream {
 
+	private PrintStream printStream;
 	private static StandardOutputStream outputStream;
 
-	private StandardOutputStream() {
+	private StandardOutputStream(PrintStream printStream) {
+		this.printStream = printStream;
 	}
 
 	public static StandardOutputStream getInstance() {
-		outputStream = new StandardOutputStream();
+		if (outputStream == null) {
+			outputStream = new StandardOutputStream(System.out);
+		}
 		return outputStream;
 	}
 
 	@Override
 	public void print(Object o) {
+		printStream.print(o);
+		printStream.flush();
 	}
 
 	@Override
 	public void println(Object o) {
+		printStream.println(o);
+		printStream.flush();
+	}
+
+	@Override
+	protected void finalize() {
+		printStream.close();
 	}
 }
