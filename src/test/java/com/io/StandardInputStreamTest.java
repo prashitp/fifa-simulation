45a45116043e14e0a8f4d03e5bc05f4644237b0e
Jay Patel <jy439129@dal.ca>
2021-11-17 23:45:43 -0400

    Refactored test cases for input and ouput stream

--- src/test/java/com/io/StandardInputStreamTest.java
+++ src/test/java/com/io/StandardInputStreamTest.java
@@ -2,35 +2,57 @@ package com.io;
 
 import java.io.ByteArrayInputStream;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * @author Jay Patel
  */
+@TestMethodOrder(OrderAnnotation.class)
 public class StandardInputStreamTest {
 
-	@Test
-	public void inputStreamTest() {
-		String userInput = String.format("hello world\n150\n10000000000\n1.25", System.lineSeparator());
+	public static IInputStream inputStream;
 
+	@BeforeAll
+	public static void init() {
+		String userInput = String.format("hello world\n150\n10000000000\n1.25", System.lineSeparator());
 		ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
 		System.setIn(bais);
+		inputStream = StandardInputStream.getInstance();
+	}
 
+	@Test
+	@Order(1)
+	public void stringInputTest() {
 		String expectedString = "hello world";
-		Integer expectedInteger = 150;
-		Long expectedLong = 10000000000L;
-		Double expectedDouble = 1.25D;
-
-		IInputStream inputStream = StandardInputStream.getInstance();
-
 		String actualString = inputStream.readLine();
-		Integer actualInteger = inputStream.readInteger();
-		Long actualLong = inputStream.readLong();
-		Double actualDouble = inputStream.readDouble();
-
 		assert (expectedString.equals(actualString));
+	}
+
+	@Test
+	@Order(2)
+	public void integerInputTest() {
+		Integer expectedInteger = 150;
+		Integer actualInteger = inputStream.readInteger();
 		assert (expectedInteger.equals(actualInteger));
+	}
+
+	@Test
+	@Order(3)
+	public void longInputTest() {
+		Long expectedLong = 10000000000L;
+		Long actualLong = inputStream.readLong();
 		assert (expectedLong.equals(actualLong));
+	}
+
+	@Test
+	@Order(4)
+	public void doubleInputTest() {
+		Double expectedDouble = 1.25D;
+		Double actualDouble = inputStream.readDouble();
 		assert (expectedDouble.equals(actualDouble));
 	}
 }
--- src/test/java/com/io/StandardOutputStreamTest.java
+++ src/test/java/com/io/StandardOutputStreamTest.java
@@ -3,29 +3,44 @@ package com.io;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * @author Jay Patel
  */
+@TestMethodOrder(OrderAnnotation.class)
 public class StandardOutputStreamTest {
 
-	@Test
-	public void outputStreamTest() {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	public static IOutputStream outputStream;
+	public static ByteArrayOutputStream baos;
+
+	@BeforeAll
+	public static void init() {
+		baos = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(baos);
 		System.setOut(printStream);
-
-		IOutputStream outputStream = StandardOutputStream.getInstance();
-
-		outputStream.println("hello");
-		outputStream.print("world");
-		String[] output = baos.toString().split(System.lineSeparator());
-
-		String firstLine = "hello";
-		String secondLine = "world";
-
-		assert (output.length == 2 && output[0].equals(firstLine) && output[1].equals(secondLine));
+		outputStream = StandardOutputStream.getInstance();
 	}
 
+	@Test
+	@Order(1)
+	public void printlnOutputTest() {
+		outputStream.println("hello");
+		String[] output = baos.toString().split(System.lineSeparator());
+		String firstLine = "hello";
+		assert (output.length == 1 && output[0].equals(firstLine));
+	}
+
+	@Test
+	@Order(2)
+	public void printOutputTest() {
+		outputStream.print("world");
+		String[] output = baos.toString().split(System.lineSeparator());
+		String secondLine = "world";
+		assert (output.length == 2 && output[1].equals(secondLine));
+	}
 }
