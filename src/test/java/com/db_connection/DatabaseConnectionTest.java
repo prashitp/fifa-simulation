77d057ebb6f9730eb4b115104c3963cb80b32a83
Mayank Sareen <my619913@dal.ca>
2021-11-19 02:45:11 -0400

    DB Connection

---
+++ src/test/java/com/db_connection/DatabaseConnectionTest.java
@@ -0,0 +1,17 @@
+package com.db_connection;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+
+public class DatabaseConnectionTest {
+    @Test
+    public void setConnectionWithDatabaseTest() {
+        Connection connection = DatabaseConnection.setConnectionWithDatabase();
+        try {
+            assertEquals(true, !connection.isClosed(), "Connection set to Database");
+        } catch (Exception ex) {
+            fail("Exception occurred"+ ex.getMessage());
+        }
+    }
+}
+
