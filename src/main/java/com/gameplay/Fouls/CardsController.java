f9d60c6d95faa54ff5d43cd7a6bc2a0f31d02019
Vasu Gamdha <vs527199@dal.ca>
2021-12-06 20:13:50 -0400

    removed main()

--- src/main/java/com/gameplay/Fouls/CardsController.java
+++ src/main/java/com/gameplay/Fouls/CardsController.java
@@ -29,21 +29,4 @@ public class CardsController implements ICardsController{
 
         return fouls;
     }
-
-    public static void main(String[] args) {
-        HashMap<PlayerModel, PlayingPosition> team1 = new HashMap<>();
-        HashMap<PlayerModel, PlayingPosition> team2 = new HashMap<>();
-        team1.put(Constants.PLAYERS[0], PlayingPosition.FORWARD);
-        team1.put(Constants.PLAYERS[12], PlayingPosition.MIDFIELDER);
-        team1.put(Constants.PLAYERS[20], PlayingPosition.DEFENDER);
-        team1.put(Constants.PLAYERS[39], PlayingPosition.GOALKEEPER);
-
-        team2.put(Constants.PLAYERS[1], PlayingPosition.FORWARD);
-        team2.put(Constants.PLAYERS[8], PlayingPosition.MIDFIELDER);
-        team2.put(Constants.PLAYERS[13], PlayingPosition.DEFENDER);
-        team2.put(Constants.PLAYERS[18], PlayingPosition.GOALKEEPER);
-
-        ICardsController cc = new CardsController(team1, team2);
-        System.out.println(cc.getFouls());
-    }
 }
