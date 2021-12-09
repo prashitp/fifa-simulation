5528790b01ed51421fd5e669fad18e3e96339625
Mayank Sareen <my619913@dal.ca>
2021-12-09 16:37:40 -0400

    database bugfix

--- src/main/java/com/database_operations/DatabaseConnection.java
+++ src/main/java/com/database_operations/DatabaseConnection.java
@@ -2,9 +2,7 @@ package com.database_operations;
 
 import com.utils.Constants;
 import com.utils.LogService;
-
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -18,10 +16,9 @@ public class DatabaseConnection {
     private Connection connection;
     private DatabaseConnection() {
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
             connection = DriverManager.getConnection(Constants.CONNECTION_DEV_URL, Constants.CONNECTION_USERNAME,
                     Constants.CONNECTION_PASSWORD);
-        } catch (SQLException | ClassNotFoundException ex) {
+        } catch (SQLException  ex) {
             logService.log(Level.SEVERE, "ERROR! Exception occurred while connecting to Database :: " + ex.getMessage());
         }
     }
