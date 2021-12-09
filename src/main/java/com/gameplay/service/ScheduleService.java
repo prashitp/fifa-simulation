f19f373ff16911dd8df0598cb4638fc5f2b44cd1
Jay Patel <jy439129@dal.ca>
2021-12-09 07:59:11 -0400

    resolved test cases error

--- src/main/java/com/gameplay/service/ScheduleService.java
+++ src/main/java/com/gameplay/service/ScheduleService.java
@@ -49,7 +49,7 @@ public class ScheduleService implements IScheduleService {
 	}
 
 	private List<MatchModel> findTotalMatchesToBePlayed(Boolean firstTeamPlayingHomeMatch) {
-		List<ClubModel> teams = teamService.fetchAllTeams();
+		List<ClubModel> teams = new ArrayList<>(teamService.fetchAllTeams());
 		Collections.shuffle(teams);
 		COUNT_TEAM = teams.size();
 		List<MatchModel> matches = new ArrayList<>();
